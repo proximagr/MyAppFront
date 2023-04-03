b22171d740fb6032748347e35d1bc7b63ae6a8d4
Pantelis Apostolidis <proximagr@hotmail.com>
2023-04-03 23:15:52 +0300

    modified:   add-user.js

--- add-user.js
+++ add-user.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (event) => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ name, email })
+      body: JSON.stringify({ name })
     });
     
     if (!response.ok) {
