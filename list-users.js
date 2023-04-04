76cda6849e71df625d48c88f7d1e513f8be005ea
Pantelis Apostolidis <proximagr@hotmail.com>
2023-04-04 14:29:18 +0300

    modified:   index.html modified:   list-users.js

--- list-users.js
+++ list-users.js
@@ -1,16 +1,18 @@
-const listUsersButton = document.querySelector("#list-users-btn");
+document.addEventListener("DOMContentLoaded", function () {
+  const listUsersButton = document.querySelector("#list-users-btn");
+  const usersTableBody = document.querySelector("#users-table tbody");
 
-listUsersButton.addEventListener("click", function () {
-  fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
-    .then((response) => response.json())
-    .then((data) => {
-      const table = document.querySelector("#customers");
-      table.innerHTML = "";
-      data.forEach((user) => {
-        const row = table.insertRow();
-        const nameCell = row.insertCell(0);
-        nameCell.innerHTML = user.name;
-      });
-    })
-    .catch((error) => console.error(error));
+  listUsersButton.addEventListener("click", function () {
+    fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
+      .then((response) => response.json())
+      .then((data) => {
+        usersTableBody.innerHTML = "";
+        data.forEach((user) => {
+          const row = usersTableBody.insertRow();
+          const nameCell = row.insertCell(0);
+          nameCell.innerHTML = user.name;
+        });
+      })
+      .catch((error) => console.error(error));
+  });
 });
