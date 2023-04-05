cb0da2aa0ec9cda18661b02e041aee11e0c3650f
Pantelis Apostolidis <proximagr@hotmail.com>
2023-04-05 13:47:24 +0300

    modified:   add-project.js

--- add-project.js
+++ add-project.js
@@ -1,5 +1,4 @@
 const customerSelect = document.getElementById("customer-select");
-const addProjectForm = document.getElementById("add-project-form");
 
 // Populate customers dropdown
 fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
@@ -16,35 +15,4 @@ fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
   .then((response) => response.json()) //logs the response to the console
   .then((data) => console.log(data)) //add logs to see what's going on
   .catch((error) => console.error(error)); //logs the error to the console
-
-// Handle form submission
-/*
-addProjectForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const projectData = {
-    customer_id: customerSelect.value,
-    project: document.getElementById("project").value,
-    price: document.getElementById("price").value,
-  };
-
-  fetch("http://arch.francecentral.cloudapp.azure.com:43704/add-project", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(projectData),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data)) //add logs to see what's going on
-    .then((data) => {
-      console.log("Success:", data);
-      alert("Project added successfully!");
-    })
-    .then((response) => response.json()) //logs the response to the console
-    .then((data) => console.log(data)) //add logs to see what's going on
-    .catch((error) => {
-      console.error("Error:", error);
-      alert("Error adding project!");
-    });
-});
-*/
+  
