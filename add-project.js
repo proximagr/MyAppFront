e55ce934dd79ccfc90fc3d7f739343ef9f57bceb
Pantelis Apostolidis <proximagr@hotmail.com>
2023-04-05 08:46:15 +0300

    modified:   add-project.js

--- add-project.js
+++ add-project.js
@@ -1,41 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const customerSelect = document.querySelector("#customer-select");
-  const addProjectForm = document.querySelector("#add-project-form");
+const customerSelect = document.getElementById("customer-select");
+const addProjectForm = document.getElementById("add-project-form");
 
-  // Populate customer select options
-  fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
+// Populate customers dropdown
+fetch("http://arch.francecentral.cloudapp.azure.com:43704/list-users")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((user) => {
+      const option = document.createElement("option");
+      option.value = user.id;
+      option.text = user.name;
+      customerSelect.add(option);
+    });
+  })
+  .catch((error) => console.error(error));
+
+// Handle form submission
+addProjectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const projectData = {
+    customer_id: customerSelect.value,
+    project: document.getElementById("project").value,
+    price: document.getElementById("price").value,
+  };
+
+  fetch("http://arch.francecentral.cloudapp.azure.com:43704/add-project", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(projectData),
+  })
     .then((response) => response.json())
     .then((data) => {
-      data.forEach((user) => {
-        const option = document.createElement("option");
-        option.value = user.id;
-        option.text = user.name;
-        customerSelect.add(option);
-      });
+      console.log("Success:", data);
+      alert("Project added successfully!");
     })
-    .catch((error) => console.error(error));
-
-  // Handle form submit
-  addProjectForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(addProjectForm);
-    const project = formData.get("project");
-    const price = formData.get("price");
-    const customer_id = formData.get("customer");
-
-    fetch("http://arch.francecentral.cloudapp.azure.com:43704/add-project", {
-      method: "POST",
-      body: JSON.stringify({ project, price, customer_id }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Project added:", data);
-        addProjectForm.reset();
-      })
-      .catch((error) => console.error(error));
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Error adding project!");
+    });
 });
