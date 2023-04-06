04115971fe8c6c82b150a77ad22f44185d8c3b31
Pantelis Apostolidis <proximagr@hotmail.com>
2023-04-06 13:20:09 +0300

    modified:   index.html modified:   list-projects.js

--- list-projects.js
+++ list-projects.js
@@ -1,50 +1,61 @@
-const xhrProjects = new XMLHttpRequest();
-xhrProjects.open('GET', 'http://arch.francecentral.cloudapp.azure.com:43704/list-projects', true);
-xhrProjects.onload = function () {
-  if (this.status === 200) {
-    const projects = JSON.parse(this.responseText);
-    const xhrCustomers = new XMLHttpRequest();
-    xhrCustomers.open('GET', 'http://arch.francecentral.cloudapp.azure.com:43704/list-users', true);
-    xhrCustomers.onload = function () {
+function listProjects() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onload = function() {
       if (this.status === 200) {
-        const customers = JSON.parse(this.responseText);
-        const xhrPayments = new XMLHttpRequest();
-        xhrPayments.open('GET', 'http://arch.francecentral.cloudapp.azure.com:43704/list-payments', true);
-        xhrPayments.onload = function () {
-          if (this.status === 200) {
-            const payments = JSON.parse(this.responseText);
-            const projectsTable = document.getElementById('projectsTable');
-            projectsTable.innerHTML = `
-              <thead>
-                <tr>
-                  <th>Project</th>
-                  <th>Price</th>
-                  <th>Customer</th>
-                  <th>Payments</th>
-                </tr>
-              </thead>
-              <tbody>
-                ${projects.map(project => {
-                  const customer = customers.find(c => c.id === project.customer_id);
-                  const projectPayments = payments.filter(p => p.project_id === project.id);
-                  const paymentSum = projectPayments.reduce((acc, curr) => acc + curr.amount, 0);
-                  return `
-                    <tr>
-                      <td>${project.project}</td>
-                      <td>${project.price}</td>
-                      <td>${customer ? customer.name : ''}</td>
-                      <td>${paymentSum}</td>
-                    </tr>
-                  `;
-                }).join('')}
-              </tbody>
-            `;
-          }
-        };
-        xhrPayments.send();
+          const projects = JSON.parse(this.responseText);
+          const tbody = document.querySelector('#project-list tbody');
+          tbody.innerHTML = '';
+
+          projects.forEach(project => {
+              // Create a new row for each project
+              const row = document.createElement('tr');
+              tbody.appendChild(row);
+
+              // Add the project name and price to the row
+              const nameCell = document.createElement('td');
+              nameCell.textContent = project.project;
+              row.appendChild(nameCell);
+
+              const priceCell = document.createElement('td');
+              priceCell.textContent = project.price;
+              row.appendChild(priceCell);
+
+              // Fetch the customer name for the project
+              const customerXHR = new XMLHttpRequest();
+              customerXHR.open('GET', `http://arch.francecentral.cloudapp.azure.com:43704/list-users/${project.customer_id}`, true);
+              customerXHR.onload = function() {
+                  if (this.status === 200) {
+                      const customer = JSON.parse(this.responseText);
+                      // Add the customer name to the row
+                      const customerCell = document.createElement('td');
+                      customerCell.textContent = customer.name;
+                      row.appendChild(customerCell);
+                  }
+              };
+              customerXHR.send();
+
+              // Fetch the payments for the project
+              const paymentXHR = new XMLHttpRequest();
+              paymentXHR.open('GET', `http://arch.francecentral.cloudapp.azure.com:43704/list-payments/${project.id}`, true);
+              paymentXHR.onload = function() {
+                  if (this.status === 200) {
+                      const payments = JSON.parse(this.responseText);
+                      let paymentSum = 0;
+                      payments.forEach(payment => {
+                          paymentSum += payment.amount;
+                      });
+                      // Add the payment sum to the row
+                      const paymentCell = document.createElement('td');
+                      paymentCell.textContent = paymentSum;
+                      row.appendChild(paymentCell);
+                  }
+              };
+              paymentXHR.send();
+          });
       }
-    };
-    xhrCustomers.send();
-  }
-};
-xhrProjects.send();
+  };
+
+  xhr.open('GET', 'http://arch.francecentral.cloudapp.azure.com:43704/list-projects', true);
+  xhr.send();
+}
